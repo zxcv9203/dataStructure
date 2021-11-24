a07ea5e6985619c878215fcf625271c48d13e82e
yongckim <zxcv9203@naver.com>
2021-11-24 23:37:37 +0900

    double linked list ing

--- chap02/doublelinkedlist/doublylist.c
+++ chap02/doublelinkedlist/doublylist.c
@@ -22,54 +22,142 @@ void deleteDoublyList(DoublyList* pList) {
 	DoublyListNode *node;
 	DoublyListNode *release;
 
+	if (pList == NULL)
+		return ;
 	node = pList->headerNode.pRLink;
-	while (node->pRLink) {
+	while (node) {
 		release = node;
-		node =
-		free()
+		node = node->pRLink;
+		free(release);
+		release = 0;
 	}
-	free(node->pRLink);
-
+	free(pList);
+	pList = 0;
 }
 
 /*
 *** Double Linked list를 position 위치에 elment의 내용으로 추가합니다.
 */
 int addDLElement(DoublyList* pList, int position, DoublyListNode element) {
+	DoublyListNode *node;
+	DoublyListNode *prev;
+	int				i;
 
+	i = 0;
+	if (pList == NULL && (pList->currentElementCount < 0 || pList->currentElementCount < position))
+		return (FALSE);
+	node = (DoublyListNode *)malloc(sizeof(DoublyListNode));
+	if (node == NULL)
+		return (FALSE);
+	node->data = element.data;
+	prev = &pList->headerNode;
+	while (i < position && prev->pRLink)
+	{
+		prev = prev->pRLink;
+		i++;
+	}
+	node->pRLink = prev->pRLink;
+	node->pLLink = prev;
+	prev->pRLink = node;
+	if (node->pRLink == NULL)
+	{
+		node->pRLink = &pList->headerNode;
+		pList->headerNode.pLLink = node;
+	}
+	pList->currentElementCount++;
+	return (TRUE);	
 }
 
 /*
 *** Double Linked list의 포지션 위치의 값을 삭제합니다.
 */
 int removeDLElement(DoublyList* pList, int position) {
+	DoublyListNode *node;
+	DoublyListNode *prev;
+	DoublyListNode *next;
 
+	int i;
+	
+	i = 0;
+	if (position >= pList->currentElementCount || position < 0)
+		return (FALSE);
+	if (pList == NULL)
+		return (FALSE);
+	prev = &pList->headerNode;
+	while (i++ < position && prev->pRLink)
+		prev = prev->pRLink;
+	node = prev->pRLink;
+	next = node->pRLink;
+	next->pLLink = prev;
+	prev = node->pRLink;
+	free(node);
+	node = 0;
+	pList->currentElementCount--;
+	return (TRUE);
 }
 
 /*
 *** Double linked list의 data를 초기화합니다.
 */
 void clearDoublyList(DoublyList* pList) {
+    DoublyListNode    *node;
+	int			i;
 
+    if (pList == NULL)
+        return ;
+    i = pList->currentElementCount;
+    node = pList->headerNode.pRLink;
+    while (i--)
+    {
+        node->data = 0;
+        node = node->pRLink;
+    }
 }
 
 /*
 *** Double Linked list의 현재 길이를 가져옵니다.
 */
 int getDoublyListLength(DoublyList* pList) {
-
+    if (pList == NULL)
+        return (FALSE);
+    return (pList->currentElementCount);
 }
 
 /*
 *** Double Linked List의 position 위치의 노드를 반환합니다.
 */
 DoublyListNode* getDLElement(DoublyList* pList, int position) {
+   DoublyListNode    *node;
 
+	if (pList == NULL)
+       return (FALSE);
+	if (position >= pList->currentElementCount || position < 0)
+       return (FALSE);
+	if (pList->currentElementCount / 2 <= position)
+	{
+		node = pList->headerNode.pLLink;
+   		while (position--)
+       		node = node->pLLink;
+	}
+	else
+	{
+		node = pList->headerNode.pRLink;
+   		while (position--)
+       		node = node->pRLink;
+	}
+   return (node);
 }
 
 /*
 *** Display Double List의 값들을 출력해줍니다.
 */
 void displayDoublyList(DoublyList* pList) {
+    DoublyListNode    *node;
 
+    node = pList->headerNode.pRLink;
+    while (node)
+    {
+        printf("[%p]\t%d\n", node, node->data);
+        node = node->pRLink;
+    }
 }
--- chap02/doublelinkedlist/main.c
+++ chap02/doublelinkedlist/main.c
@@ -1,9 +1,35 @@
 #include "doublylist.h"
 
 void createDoublyList_test() {
+	DoublyList *dl;
+
+	dl = createDoublyList();
+	printf("%d %d %p %p\n", dl->currentElementCount, dl->headerNode.data, dl->headerNode.pLLink, dl->headerNode.pRLink);
+}
+
+void addDLElement_test() {
+	DoublyList *dl;
+	DoublyListNode element;
+	DoublyListNode *node;
+
+	dl = createDoublyList();
+	for (int i = 0; i < 2; i++) {
+		element.data = (i+1) * 3;
+		addDLElement(dl, i, element);
+	}
+	node = dl->headerNode.pRLink;
+	for (int i = 0; i < 10; i++) {
+		printf("prev : %p node : %p %d next : %p\n", node->pLLink, node, node->data, node->pRLink);
+		node = node->pRLink;
+	}
+}
+
+void removeDLElement_test() {
 
 }
 
 int main() {
-	createDoublyList_test();
+	//createDoublyList_test();
+	addDLElement_test();
+	//removeDLElement_test();
 }
